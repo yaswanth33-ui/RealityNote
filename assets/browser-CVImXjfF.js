fc770307f3aafd4a15c83903fbe8835ea9080a56
Yaswanth Reddy <yaswanthreddypanem@gmail.com>
2025-03-13 11:09:53 +0530

    Updates

---
+++ assets/browser-CVImXjfF.js
@@ -0,0 +1,1 @@
+import{f as e}from"./vendor-DB1mM43M.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
